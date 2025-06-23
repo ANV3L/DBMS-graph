bbcab8416423a6649fd39f0ec956da755ff46311
Kirill <conic648374@gmail.com>
2025-06-23 21:14:31 +0300

    Finished Terminal::run

--- include/terminal.h
+++ include/terminal.h
@@ -3,11 +3,15 @@
 
 #include <iostream>
 #include <string>
+#include <map>
 
 class Terminal{
 public:
     Terminal();
     void run();
+private:
+    std::map<std::string, int (Terminal::*)()> commands;
+    int exit();
 };
 
 
--- src/terminal.cpp
+++ src/terminal.cpp
@@ -1,13 +1,40 @@
 #include "../include/terminal.h"
 
-Terminal::Terminal() {};
+Terminal::Terminal(){
+    commands["exit"] = &Terminal::exit; // Здесь добавлять новые функции
+};
 
-void Terminal::run() {
+void Terminal::run(){
+    int res = 0;
     while(1){
+        res = 0;
         std::string comand;
-        std::cout << "> ";
+        std::cout << "> ";  // На данном участке принимаются команды
         std::getline(std::cin, comand);
-        if (comand == "exit")return;
-        //Здесь реализовать распределение по командам
+        
+        auto it = commands.find(comand);
+        if(it != commands.end()){
+            res = (this->*(it->second))();  // Если находим команду в словаре, то запускаем её
+        } else {
+            res = -2;
+        }
+        switch(res){ // Обработка команд
+            case 0:
+                break;
+            case -1:
+                return;
+            case -2:
+                std::cout << "Incorrect comand" << std::endl;
+                break;
+            default:
+                std::cout <<"Uncategorized issue" << std::endl;
+                break;
+        }
+
     }
+}
+
+
+int Terminal::exit(){
+    return -1;
 }
